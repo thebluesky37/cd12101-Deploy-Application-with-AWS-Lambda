64f30b7ed4f648229a147cfdd3da87838fff8736
thebluesky37 <thebluesky37@users.noreply.github.com>
2023-11-08 10:31:48 +0700

    Validate request

--- starter/backend/src/lambda/http/createTodo.ts
+++ starter/backend/src/lambda/http/createTodo.ts
@@ -10,23 +10,44 @@ import { createTodo } from '../../businessLogic/todo';
 import { TodoCreate } from '../../models/todo';
 import { getToken } from '../../auth/utils';
 
+// Import AWS X-Ray
+import * as AWSXRay from 'aws-xray-sdk';
+AWSXRay.captureAWS(require('aws-sdk'));
+
 const createTodoHandler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
+  // Create an X-Ray segment
+  const segment = AWSXRay.getSegment();
+  const subsegment = segment.addNewSubsegment('createTodoHandler');
+  subsegment.addAnnotation('HTTPMethod', "POST");
   console.log('Processing event: ', event);
-  const jwtToken = getToken(event);
 
-  const newTodo: TodoCreate = JSON.parse(event.body);
-  const item = await createTodo(jwtToken, newTodo);
-  console.log(item);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      item: item,
-    }),
-  };
+  try {
+    const jwtToken = getToken(event);
+    const newTodo: TodoCreate = JSON.parse(event.body);
+    if (!newTodo.name || newTodo.name.trim() === '') {
+      throw new Error('Todo name cannot be empty.');
+    }
+    const item = await createTodo(jwtToken, newTodo);
+    console.log(item);
+
+    // Close the X-Ray subsegment
+    subsegment.close();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        item: item,
+      }),
+    };
+  } catch (error) {
+    // Add error to X-Ray subsegment
+    subsegment.addError(error);
+    // Close the X-Ray subsegment
+    subsegment.close();
+    throw error;
+  }
 };
 
-export const handler = middy(createTodoHandler).use(
-  cors({ credentials: true }),
-);
+export const handler = middy(createTodoHandler).use(cors({ credentials: true }));
